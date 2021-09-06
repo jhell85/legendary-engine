97089789f4d489351cd8e4ac033846396dab424b
jhell85 <jhell85@gmail.com>
2021-09-05 17:23:45 -0700

    deleted unused app

--- api/urls.py
+++
@@ -1,6 +0,0 @@
-from api.views import testView
-from django.urls import path
-
-urlpatterns = [
-    path('', testView.as_view(), name='home')
-]
--- api/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.views import generic
-
-class testView(generic.CreateView):
-  template_name = 'home.html'
