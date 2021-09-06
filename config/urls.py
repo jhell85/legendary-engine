de21a8a656f5b75d5d2b4b19d1f057acbda05363
jhell85 <jhell85@gmail.com>
2021-09-05 17:25:44 -0700

    add url path for front end

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http.response import HttpResponse
 from django.urls import path, include
+from django.shortcuts import render
+
+vue_urls=[path('', lambda request: HttpResponse(render(request, 'vue_index.html')))]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    # path('api/', include('api.urls'))
+    path('', include(vue_urls))
 ]
